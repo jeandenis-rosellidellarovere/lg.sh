e20ac0488af5f2d211aa5df2bdefde7012159315
jeandenis-rosellidellarovere <65887482+jeandenis-rosellidellarovere@users.noreply.github.com>
2020-06-12 11:32:16 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,7 +2,13 @@
 
 dialog --title "Latino&Greco - installazione" --textbox messaggio.txt 20 80
 mkdir /home/$USER/lettereclassiche && cd /home/$USER/lettereclassiche
-wget #ilmiolinkagoogledrive
+wget -r http://www.poesialatina.it/_ns/ProsaLat/p/mappa.html
+wget -r http://www.poesialatina.it/_ns/Testi/p/mappa.html
+wget -r http://www.poesialatina.it/_ns/Greek/testi/p/MappaProsaGr.html
+mv /home/$USER/lettereclassiche/www.poesialatina.it/_ns/Greek/Testi/* /home/$USER/lettereclassiche
+mv /home/$USER/lettereclassiche/www.poesialatina.it/_ns/ProsaLat/* /home/$USER/lettereclassiche
+mv /home/$USER/lettereclassiche/www.poesialatina.it/_ns/Testi/* /home/$USER/lettereclassiche
+rm -r /home/jeande/$USER/lettereclassiche/www.poesialatina.it
+sudo chmod +x /home/$USER/programmi/lg.sh/lg.sh 
+sudo cp /home/$USER/programmi/lg.sh/lg.sh /bin/lg
 dialog --title "Latino&Greco - installazione" --textbox messaggio2.txt 20 80
-sudo chmod +x lg.sh 
-sudo cp lg.sh /bin/lg
